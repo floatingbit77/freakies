19e6b600e72729fd5c768c19ca985f581983ce26
Abi Taylor <71341663+floatingbit77@users.noreply.github.com>
2022-01-07 22:23:08 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import PageFlip from "./components/PageFlip/PageFlip";
+import MapShow from "./components/Map/Map";
+
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+     
+      <PageFlip />
+      <MapShow />
+    </div>
+  );
+}
